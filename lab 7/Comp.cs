70e7b9f11add8702dc142df0c2c08c3513546c3a
A-KB4 <kabachii.artem@gmail.com>
2024-06-09 16:27:33 +0300

    7

---
+++ lab 7/Comp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab_7
+{
+    public class Comp : IComparable
+    {
+        public string Name { get; set; }
+        public string Os { get; set; }
+        public string Virob { get; set; }
+        public int Proc { get; set; }
+        public double Bios { get; set; }
+        public double OperMemory { get; set; }
+        public bool Inch { get; set; }
+        public bool Overclocking { get; set; }
+
+        public string Info()
+        {
+            return Name + ", " + Os + ", " + Virob;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Comp t = obj as Comp;
+            return string.Compare(this.Name, t.Name);
+        }
+
+        public Comp()
+        {
+
+        }
+
+        public Comp(string name, string os, string virob, int proc, double bios, double operMemory, bool inch, bool overclocking)
+        {
+            Name = name;
+            Os = os;
+            Virob = virob;
+            Proc = proc;
+            Bios = bios;
+            OperMemory = operMemory;
+            Inch = inch;
+            Overclocking = overclocking;
+        }
+    }
+}
---
+++ lab 7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace lab_7
+{
+    class Program
+    {
+        static List<Comp> comps;
+
+        static void Main(string[] args)
+        {
+            comps = new List<Comp>();
+
+            // Читання з файлу і додавання комп'ютерів в список comps
+            FileStream fs = new FileStream("Comp.comps", FileMode.Open);
+            BinaryReader reader = new BinaryReader(fs);
+            try
+            {
+                Console.WriteLine("Зчитування даних з файлу...\n");
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    Comp comp = new Comp();
+                    comp.Name = reader.ReadString();
+                    comp.Os = reader.ReadString();
+                    comp.Virob = reader.ReadString();
+                    comp.Proc = reader.ReadInt32();
+                    comp.Bios = reader.ReadDouble();
+                    comp.OperMemory = reader.ReadDouble();
+                    comp.Inch = reader.ReadBoolean();
+                    comp.Overclocking = reader.ReadBoolean();
+
+                    comps.Add(comp);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Сталася помилка: {0}", ex.Message);
+            }
+            finally
+            {
+                reader.Close();
+            }
+
+            // Виведення списку комп'ютерів на екран
+            Console.WriteLine("Кількість комп'ютерів: {0}", comps.Count);
+            foreach (var comp in comps)
+            {
+                Console.WriteLine(comp.Info());
+            }
+
+            // Затримка перед завершенням програми
+            Console.ReadKey();
+        }
+    }
+}
